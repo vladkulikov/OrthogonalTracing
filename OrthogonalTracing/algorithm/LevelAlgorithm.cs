bd3d8a604ef37790b399e26ae699b8af88ab8555
Maxim Godovicyn <maxim.godovicyn@gmail.com>
2019-10-08 21:13:03 +0300

    Added func for solution check

--- OrthogonalTracing/algorithm/LevelAlgorithm.cs
+++ OrthogonalTracing/algorithm/LevelAlgorithm.cs
@@ -18,7 +18,54 @@ namespace algorithm
 
         public bool checkSolution(Net net, Solution solution)
         {
-            throw new NotImplementedException();
+            int[][] tracks = solution.Tracks;
+            int[][] pins = solution.Pins;
+            int[][] graph = net.Graph;
+            bool result = true;
+
+            if (tracks.Length != pins.Length)
+            {
+                Console.WriteLine("tracks.Length != pins.Length");
+                return false;
+            }
+
+
+            //TODO: add num of not traced pins
+            for (int i = 0; i < tracks.Length; i++)
+            {
+                if (tracks[i] != null)
+                {
+                    List<int> cur = new List<int>(graph[ pins[i][0] ]);
+                    if (cur.IndexOf(tracks[i][0]) < 0)
+                    {
+                        Console.WriteLine("Error in track {0}, on benween pos 0 and 1. Edge doesn't exist", i);
+                        result = false;
+                    }
+
+                    int len = tracks[i].Length;
+                    for (int j = 0; j < len - 1; j++)
+                    {
+                        cur = new List<int>(graph[tracks[i][j]]);
+                        if (cur.IndexOf(tracks[i][j + 1]) < 0)
+                        {
+                            Console.WriteLine("Error in track {0}, on pos {1}, benween {2} and {3}. Edge doesn't exist", i, j, tracks[i][j], tracks[i][j + 1]);
+                            result = false;
+                        }
+                    }
+                    cur = new List<int>(graph[ tracks[i][len - 1] ]);
+                    if (cur.IndexOf(pins[i][1]) < 0)
+                    {
+                        Console.WriteLine("Error in track {0}, on pos {1}, benween {2} and {3}. Edge doesn't exist", i, len, tracks[i][len - 1], pins[i][1]);
+                        result = false;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Track {0} is not traced", i);
+                    //result = ?
+                }
+            }
+            return result;
         }
     }
 }
