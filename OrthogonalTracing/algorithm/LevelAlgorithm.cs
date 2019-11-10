3448428f163cb5272abcdf4e2eb80d2fdf5ae043
vladkulikov <31815075+vladkulikov@users.noreply.github.com>
2019-11-10 18:24:32 +0300

    Add files via upload

--- OrthogonalTracing/algorithm/LevelAlgorithm.cs
+++ OrthogonalTracing/algorithm/LevelAlgorithm.cs
@@ -66,6 +66,59 @@ namespace algorithm
                 }
             }
             return result;
-        }
-    }
+		}
+
+		public static Solution searchSolution(Net net) {
+			int[][] pins = net.Pins;
+			int[][] tracks = new int[net.Pins.Length][];
+			int[][] graph = net.Graph;
+			int a = graph.Length;
+			int[,] vers = new int[2,a];
+			int[] optway = new int[a];
+			for (int i = 0; i < a; i++)
+			{
+				vers[0, i] = i;
+				vers[1, i] = -1;
+				optway[i] = 0; 
+			}
+			
+			int pin1 = pins[0][0];//пины которые нуно соединить
+			int pin2 = pins[0][1];//пины которые нуно соединить
+			int d;
+			//vers[1, 0] = 0;
+			for (int i = 0; i < a; i++)                        
+			{
+				if (graph[i].Length != 0)
+				{
+					d = vers[1, i] + 1;
+					for (int j = 0; j < graph[i].Length; j++)
+					{
+						if (vers[1, graph[i][j]] == -1)
+						{
+							vers[1, graph[i][j]] = d;
+						}
+
+					}
+				}
+			}
+			int v = vers[1, pin1];
+			int k = 0;
+			for (int j = pin1; j != pin2; )						
+			{
+				for (int i = 0; i < graph[j].Length; i++)	
+				{
+					if (vers[1, graph[j][i]] == v - 1) { optway[k] = graph[j][i]; break; }								
+				}
+				j = optway[k];
+				v = vers[1, optway[k]];
+				k++;
+			}													
+			
+
+			return new Solution(pins, tracks);
+		}
+
+		
+
+	}
 }
