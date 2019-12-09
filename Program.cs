6878beff5e092544e6c63ac2089093be7e97365d
vladkulikov <31815075+vladkulikov@users.noreply.github.com>
2019-12-09 22:02:01 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using core;
+using algorithm;
+
+
+namespace OrthogonalTracing
+{
+    class Program
+    {
+		
+
+        static void Main(string[] args)
+        {
+
+
+            Net test = Parser.getData("/media/vldad/708A70F28A70B5E6/testproject.txt");
+            Solution decision = Parser.getSolution("/media/vldad/708A70F28A70B5E6/testproject.txt");
+            //Net  test = Parser.getData("C:/Users/USER/Desktop/OrthogonalTracing-WaveMethod/OrthogonalTracing/testproject.txt");
+            //Solution decision = Parser.getSolution("C:/Users/USER/Desktop/OrthogonalTracing-WaveMethod/OrthogonalTracing/testproject.txt");
+			LevelAlgorithm alg = new LevelAlgorithm();
+			Solution sol = alg.trace(test);
+			for (int i = 0; i < sol.Tracks[0].Length; i++)
+			{
+				Console.WriteLine(sol.Tracks[0][i]);
+			}
+            
+
+			Console.ReadKey();
+        }
+    }
+}
